0fab8ead12a42ec5b26dab5e8884459ab97b2bcb
Krosara <kristian.savov@gmail.com>
2021-10-07 18:53:30 +0200

    frontend init

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import Players from './components/ProfileAPI';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Players />
+    </div>
+  );
+}
+
+export default App;
