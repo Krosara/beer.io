fc2801a7671b8a0a9a6f6acaef6be2119f5ff767
Krosara <kristian.savov@gmail.com>
2021-11-01 19:08:57 +0100

    changed country input to drop down menu

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,9 +1,13 @@
 import { playerAPI } from '../api';
-import { useState, useMemo } from 'react';
+import { Fragment, useState, useMemo } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import Select from 'react-select';
 import countryList from 'react-select-country-list';
-import { DropDownList } from '@progress/kendo-react-dropdowns';
+import { Listbox } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
 const Register = () => {
   const handleOnSubmit = (e) => {
@@ -20,7 +24,6 @@ const Register = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const history = useHistory();
-  const [country1, setCountry1] = useState('');
 
   const countries = useMemo(() => countryList().getData(), []);
 
@@ -50,7 +53,7 @@ const Register = () => {
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Email"
         />
-        <input
+        {/* <input
           className="text-center bg-tbgreen-default placeholder-white placeholder-opacity-60 h-9 w-full hover:bg-tbgreen-hover focus:bg-tbgreen-hover focus:outline-none focus:placeholder-transparent"
           type="text"
           name="country"
@@ -58,7 +61,61 @@ const Register = () => {
           value={country}
           onChange={(e) => setCountry(e.target.value)}
           placeholder="Country"
-        />
+        /> */}
+        <Listbox value={country} onChange={setCountry}>
+          <Listbox.Button className="text-center bg-tbgreen-default placeholder-white placeholder-opacity-60 h-9 w-full hover:bg-tbgreen-hover focus:bg-tbgreen-hover focus:outline-none focus:placeholder-transparent">
+            <span className=" text-center">{country.label}</span>
+            <SelectorIcon
+              className=" h-5 w-5 float-right mr-2 text-gray-300"
+              aria-hidden="true"
+            />
+          </Listbox.Button>
+
+          <Listbox.Options className="z-10 mt-1 w-full max-h-48 bg-btngreen-dark text-offwhite overflow-auto ">
+            {countries.map((country) => (
+              <Listbox.Option
+                key={country.label}
+                className={({ active }) =>
+                  classNames(
+                    active ? 'text-white bg-tbgreen-hover' : 'text-offwhite',
+                    'select-none relative py-2 pl-3 pr-9'
+                  )
+                }
+                value={country}
+              >
+                {({ selected, active }) => (
+                  <>
+                    <div className="flex items-center">
+                      <span
+                        className={classNames(
+                          selected ? 'font-bold' : 'font-normal',
+                          'ml-3 block truncate'
+                        )}
+                      >
+                        {country.label}
+                      </span>
+                    </div>
+
+                    {selected ? (
+                      <span
+                        className={classNames(
+                          active ? 'text-white' : 'text-indigo-600',
+                          'absolute inset-y-0 right-0 flex items-center pr-4'
+                        )}
+                      >
+                        <CheckIcon
+                          className="h-5 w-5 text-offwhite"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    ) : null}
+                  </>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Listbox>
+
         <input
           className="text-center bg-tbgreen-default placeholder-white placeholder-opacity-60 h-9 w-full hover:bg-tbgreen-hover focus:bg-tbgreen-hover focus:outline-none focus:placeholder-transparent"
           type="password"
