6e6f24015bb31f31bfc302c9114b33f371144644
Krosara <kristian.savov@gmail.com>
2022-01-20 12:27:18 +0100

    chat working

--- backend/src/main/java/com/savov/beer_io/dto/MessageDTO.java
+++ backend/src/main/java/com/savov/beer_io/dto/MessageDTO.java
@@ -5,4 +5,5 @@ import lombok.Data;
 @Data
 public class MessageDTO {
     private String messageContent;
+    private String user;
 }
--- backend/src/main/java/com/savov/beer_io/dto/ResponseMessageDTO.java
+++ backend/src/main/java/com/savov/beer_io/dto/ResponseMessageDTO.java
@@ -7,4 +7,5 @@ import lombok.Data;
 @AllArgsConstructor
 public class ResponseMessageDTO {
     private String content;
+    private String user;
 }
